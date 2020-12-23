cb282df8f3f80f7d3e6e8f645a16261483822955
mohamed Anis Bouaziz <mohamedanis.bouaziz@esprit.tn>
2020-12-23 13:56:30 +0100

    django-heroku

--- Customer_Churn/settings.py
+++ Customer_Churn/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
+django_heroku.settings(locals())
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
