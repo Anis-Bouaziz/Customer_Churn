5e0fe624a6e097b12a55356077b17a07f3926493
mohamed Anis Bouaziz <mohamedanis.bouaziz@esprit.tn>
2020-12-23 12:35:58 +0100

    updated reqs

--- Customer_Churn/settings.py
+++ Customer_Churn/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
