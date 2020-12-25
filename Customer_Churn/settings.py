004c8af795fad6e6b24fc288d5c8052007b392c9
mohamed Anis Bouaziz <mohamedanis.bouaziz@esprit.tn>
2020-12-25 15:29:02 +0100

    bug fix

--- Customer_Churn/settings.py
+++ Customer_Churn/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'oe*n4o9_f!7n%04(&h-0_lsoy%bsex80m(_ps0qanswmzdu2e4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['predicting-customer-churn.herokuapp.com']
 
